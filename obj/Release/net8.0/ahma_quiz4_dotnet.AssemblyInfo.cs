af4949edf182e7f3b48288ac172cda79d668d0c0
Ahmad Al Bouchi <aalbouch@my.centennialcollege.ca>
2026-04-06 09:32:56 -0400

    Added database access message to web app

--- obj/Release/net8.0/ahma_quiz4_dotnet.AssemblyInfo.cs
+++ obj/Release/net8.0/ahma_quiz4_dotnet.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ahma_quiz4_dotnet")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ee00b0c609cf27ec5fda98def7908ff815227e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ahma_quiz4_dotnet")]
